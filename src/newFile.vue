cff14a712271c59d706b3b3475f8dc5ca0bf8e7b
zou-change <760752175@qq.com>
2022-09-13 18:57:50 +0800

    tet2

---
+++ src/newFile.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        dsagads
+        <p>dgdsgs</p>
+        <span>dgdsgs</span>
+    </div>
+</template>
+<script>
+
+export default 
+{
+    data() {
+        return {
+            value1:'dfadsfs'
+        }
+    },
+}
+</script>
